56ef3e58266a8f72b9869c94f50ceb970e562404
Stijn Verdenius <stijn@verdenius.com>
2019-09-16 13:36:22 +0200

    encoder finished

--- models/generators/BaseVAE.py
+++ models/generators/BaseVAE.py
@@ -7,38 +7,50 @@ from models.GeneralModel import GeneralModel
 
 class Encoder(nn.Module):
 
-    def __init__(self, hidden_dim, z_dim, device="cpu"):
+    def __init__(self, input_dim, hidden_dim, z_dim, device="cpu"):
         super(Encoder, self).__init__()
         self.hidden_dim = hidden_dim
         self.z_dim = z_dim
 
         self.activation = nn.ReLU().to(device)
 
+        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, dropout=0.1, bias=True)
+
         self.layers = nn.Sequential(
-            # todo
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
         ).to(device)
 
         self.layer_mu = nn.Linear(hidden_dim, z_dim).to(device)
         self.layer_sig = nn.Linear(hidden_dim, z_dim).to(device)
 
     def forward(self, x):
-        shared = self.forward(x)
+        lstm_output = self.lstm.forward(x)[0]
+        shared = self.layers.forward(lstm_output)
 
         mean = self.layer_mu(shared)
         std = self.activation(self.layer_sig(shared))
 
         return mean, std
 
+import os
+
+
+
+
 
 class Decoder(nn.Module):
 
-    def __init__(self, hidden_dim, z_dim, device="cpu"):
+    def __init__(self, n_in, hidden_dim, z_dim, device="cpu"):
         super(Decoder, self).__init__()
         self.hidden_dim = hidden_dim
         self.z_dim = z_dim
 
         self.layers = nn.Sequential(
-            # todo
+            # nn.Conv1d(...),
+            # nn.Softmax(),
+            # nn.Conv1d(...),
+            # nn.Softmax(),
         ).to(device)
 
     def forward(self, x):
@@ -54,10 +66,10 @@ class BaseVAE(GeneralModel):
         self.hidden_dim = hidden_dim
         self.z_dim = z_dim
         self.device = device
-        self.encoder = Encoder(hidden_dim, z_dim, device=device)
-        self.decoder = Decoder(hidden_dim, z_dim, device=device)
+        self.encoder = Encoder(n_channels_in, hidden_dim, z_dim, device=device)
+        self.decoder = Decoder(n_channels_in, hidden_dim, z_dim, device=device)
 
-    def forward(self, x: torch.Tensor): # todo: revisit
+    def forward(self, x: torch.Tensor):  # todo: revisit
         # ensure device
         x = x.to(self.device)
 
@@ -86,7 +98,7 @@ class BaseVAE(GeneralModel):
         return elbo
 
     @staticmethod
-    def elbo(mean, std, reconstructed_mean, x): # todo: revisit
+    def elbo(mean, std, reconstructed_mean, x):  # todo: revisit
         """
         calculates negated-ELBO loss
         :param mean:
@@ -103,7 +115,18 @@ class BaseVAE(GeneralModel):
         loss_reg = torch.sum(torch.sum(-1 * torch.log(std + 1e-7) + ((std.pow(2) + mean.pow(2)) - 1) * 0.5, dim=1),
                              dim=0)
         # reconstruction loss
-        loss_recon = pytorch_functions.binary_cross_entropy(reconstructed_mean, x, reduction="sum")
+        loss_recon = pytorch_functions.cross_entropy(reconstructed_mean, x, reduction="sum")
 
         # average over batch size
         return (loss_recon + loss_reg) / batch_size
+
+
+if __name__ == '__main__':
+    testbatch = torch.randn((128, 20, 100))
+
+    vae = BaseVAE(n_channels_in=100)
+    vae: BaseVAE
+    x = vae.encoder.forward(testbatch)
+    # print(x.shape)
+    print(os.pathsep)
+
