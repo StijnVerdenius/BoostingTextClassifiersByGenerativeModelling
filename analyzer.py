6a80dae88a03457475c30aba58d7ae9bd6580e4d
ktodorov <kztodorov@outlook.com>
2019-09-30 14:11:22 +0200

    Adds F1 metric calculation

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,8 @@ from utils.system_utils import setup_directories, save_codebase_of_run
 from utils.model_utils import calculate_accuracy
 from torch.utils.data import DataLoader
 from utils.constants import *
-
+from sklearn import metrics
+from typing import List
 
 class Analyzer:
     # input: both network models
@@ -24,6 +25,23 @@ class Analyzer:
     def soft_voting(self, probs1, probs2):
         _, predictions = ((probs1 + probs2) / 2).max(dim=-1)
         return predictions
+        
+    def calculate_metrics(
+        self,
+        targets: List,
+        predictions: List,
+        average: str = "weighted"):
+
+        if sum(predictions) == 0:
+            return 0, 0, 0
+
+        precision = metrics.precision_score(
+            targets, predictions, average=average)
+        recall = metrics.recall_score(targets, predictions, average=average)
+        f1 = metrics.f1_score(targets, predictions, average=average)
+
+        return f1, precision, recall
+
 
     def analyze_misclassifications(self, test_logs):
 
@@ -35,6 +53,7 @@ class Analyzer:
         _, combined_predictions = combined_scores.max(dim=-1)
         _, classifier_predictions = classifier_scores.max(dim=-1)
         _, vaes_predictions = vaes_scores.max(dim=-1)
+
 
         # combined_predictions = self.soft_voting(vaes_scores, classifier_scores)
         # print('targets', targets)
@@ -55,6 +74,16 @@ class Analyzer:
               '\n-Classify By Elbo:', vaes_compare.float().mean().item())
 
         classifier_misfire_indices = (classifier_compare == 0).nonzero()  # get misclassifications
+
+        targets = targets.detach().tolist()
+        combined_predictions = combined_predictions.tolist()
+        classifier_predictions = classifier_predictions.tolist()
+        vaes_predictions = vaes_predictions.tolist()
+
+        combined_f1, combined_precision, combined_recall = self.calculate_metrics(targets, combined_predictions)
+        classifier_f1, classifier_precision, classifier_recall = self.calculate_metrics(targets, classifier_predictions)
+
+        print(f'Combined F1: {combined_f1}\nClassifier F1: {classifier_f1}')
 
         # check if combination correctly classified these? check how many
         # print(combined_compare[classifier_misfire_indices])
